aaf5ae67f6b18b145dfd2f2f98a5fb9752fb9e9f
jutin0852 <jutindikonu8@gmail.com>
2023-03-11 17:28:02 +0100

    progress

--- src/components/homevids.js
+++ src/components/homevids.js
@@ -8,7 +8,7 @@ export default function Homevids({mysrc,name}){
          {active ? (<p className="picName" >{name}</p>): (null)}
          {active ? <i className="material-icons arrow">call_made</i> : (null)}
          <div className="container" >
-            <video src={mysrc}  autoPlay loop muted  alt="ff" />
+            <video src={mysrc} autoPlay loop muted  alt="ff" />
         </div> 
        </div>
     )}
